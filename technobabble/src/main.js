ebda83aad51966ad06b7ea0b4d1ef17ec1040a40
mythguy1226 <robbiereddick@aol.com>
2022-01-22 18:04:34 -0500

    Finished Technobabble 4

--- technobabble/src/main.js
+++ technobabble/src/main.js
@@ -6,50 +6,77 @@ window.onload = main;
 // Function for main code
 function main()
 {
-    const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
-    
-    const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
-    
-    const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
-    
-    // Function for getting random elements from an array
-    function getRandomElement(array)
+    loadTextXHR();
+}
+
+// Function for getting random elements from an array
+function getRandomElement(array)
+{
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+// Function for generating new babble text
+function generateTechno(num, words1, words2, words3)
+{
+    let fullPhrase = "";
+    for(let i = 0; i < num; i++)
     {
-        return array[Math.floor(Math.random() * array.length)]
+        // Get 3 new random words
+        let word1 = getRandomElement(words1);
+        let word2 = getRandomElement(words2);
+        let word3 = getRandomElement(words3);
+
+        // Form the new phrase
+        let babblePhrase = word1 + " " + word2 + " " + word3;
+        fullPhrase += babblePhrase + "<br>";
     }
 
-    // Function for generating new babble text
-    function generateTechno(num)
-    {
-        let fullPhrase = "";
-        for(let i = 0; i < num; i++)
-        {
-            // Get 3 new random words
-            let word1 = getRandomElement(words1);
-            let word2 = getRandomElement(words2);
-            let word3 = getRandomElement(words3);
+    // Change the output HTML text
+    document.querySelector("#output").innerHTML = fullPhrase;
+}
 
-            // Form the new phrase
-            let babblePhrase = word1 + " " + word2 + " " + word3;
-            fullPhrase += babblePhrase + "<br>";
+// Function that loads text
+function loadTextXHR()
+{
+    // Init variables
+    const url = "data/babble-data.csv"
+    const xhr = new XMLHttpRequest();
+
+    // XHR loading  handler
+    xhr.onload = (e) => {
+        console.log(`In onload - HTTP Status Code = ${e.target.status}`);
+        const text = e.target.responseText;
+        console.log(`Success - the file length is ${text.length}`);
+        //const lines = text.split("\n");
+        //const dogNames = lines[0].split(",");
+        //const catNames = lines[1].split(",");
+
+        // Get the names for dogs, cats, and birds
+        let [words1,words2,words3] = text.split("\n");
+        words1 = words1.split(",");
+        words2 = words2.split(",");
+        words3 = words3.split(",");
+
+        // Generate some initial text
+        generateTechno(1, words1, words2, words3);
+
+        // Add the event listener to the button for generating new text
+        const button = document.querySelector("#myButton");
+        button.onclick = function() {
+            generateTechno(1, words1, words2, words3);
+        };
+
+        // Add event listener for multi-babble calls
+        const multiButton = document.querySelector("#technoFive");
+        multiButton.onclick = function() {
+            generateTechno(5, words1, words2, words3);
         }
-
-        // Change the output HTML text
-        document.querySelector("#output").innerHTML = fullPhrase;
-    }
-
-    // Generate some initial text
-    generateTechno(1);
-
-    // Add the event listener to the button for generating new text
-    const button = document.querySelector("#myButton");
-    button.onclick = function() {
-        generateTechno(1);
     };
 
-    // Add event listener for multi-babble calls
-    const multiButton = document.querySelector("#technoFive");
-    multiButton.onclick = function() {
-        generateTechno(5);
-    }
+    // XHR error handler
+    xhr.onerror = e => console.log(`In onerror - HTTP Status Code = ${e.target.status}`);
+
+    // GET the data from the url
+    xhr.open("GET", url);
+    xhr.send();
 }
